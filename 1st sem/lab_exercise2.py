96219fbf65e9c6794afee7919242aa9c06e3170f
venneveni <jovenlindongan@gmail.com>
2026-02-11 00:01:30 +0800

    user input system

---
+++ 1st sem/lab_exercise2.py
@@ -0,0 +1,11 @@
+userName = str(input("Fullname: "))
+print(type(userName))
+
+userAge = int(input("\nAge: "))
+print(type(userAge))
+
+userHeight = float(input("\nHeight in meters: "))
+print(type(userHeight))
+
+userOccupation = bool(input("\nCurrently working? "))
+print(type(userOccupation))
