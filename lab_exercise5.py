31aec317924fa301dc3816579711df9a69dbfb6e
venneveni <jovenlindongan@gmail.com>
2026-02-20 20:37:40 +0800

    definition function closest to specific number

---
+++ lab_exercise5.py
@@ -0,0 +1,18 @@
+# def closest_to_500():
+#     if abs(500 - num1) < abs(500 - num2) and abs(500 - num1) < abs(500 - num3):
+#         print(f"{num1} is closest to 500.")
+#     elif abs(500 - num2) < abs(500 - num1) and abs(500 - num2) < abs(500 - num3):
+#         print(f"{num2} is closest to 500.")
+#     elif abs(500 - num3) < abs(500 - num1) and abs(500 - num3) < abs(500 - num2):
+#         print(f"{num3} is closest to 500.")
+#     else:
+#         print(f"Number1: {num1}, number2: {num2}, and number3: {num3} are equally close to 500.")
+#     return
+
+# print("Find the closest number to 500.")
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# num3 = int(input("Enter third number: "))
+
+# closest_to_500()
+
